8d61d6dbd984c702b356f47418ac72454f462a44
Aleksandar Milenkovic <38843286+BeAllAround@users.noreply.github.com>
2025-03-31 15:17:08 +0200

    Update generate.py

--- Understanding Switch Case Statements/src/generate.py
+++ Understanding Switch Case Statements/src/generate.py
@@ -1,15 +1,27 @@
-if __name__ == '__main__':
-  code = '''
+
+def gen_static_array():
+    out = 'static int array[] = {'
+    for i in range(0, 10000):
+        out += str(i) + ','
+    out += '};'
+    print(out)
+
+def generate_switch():
+    code = '''
 void __switch_test(unsigned int type) {
   volatile int r; // NOTE: preventing compiler from optimizing it out
 
   switch(type) {
   ''';
-  for i in range(0, 100000):
-      code += 'case ' + str(i) + ':{ \n' + ' r=' + str(i) + ';\n break;\n}\n'
-  
-  code += 'default: {\n r = 11;\n break;\n}\n'
-  code += '}'
-  code += '}'
+    for i in range(0, 100000):
+        code += 'case ' + str(i) + ':{ \n' + ' r=' + str(i) + ';\n break;\n}\n'
 
-  print(code)
+    code += 'default: {\n r = 11;\n break;\n}\n'
+    code += '}'
+    code += '}'
+
+    print(code)
+
+if __name__ == '__main__':
+    # gen_static_array()
+    generate_switch()
