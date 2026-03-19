00e191ab85d1c5964ac7956c63f9750c40f98d50
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2026-03-19 20:02:58 +0100

    update

--- Recursion into Goto Labels/main.cpp
+++ Recursion into Goto Labels/main.cpp
@@ -87,10 +87,7 @@ void test_0() {
 
 _recursive_label: // void _recursive_label(size_t n);
   {
-    // __label__ _body;
-
-    // if(stack[stack.size()-1].n == _end) {
-    if(call_stack[stack_p - 1].n == _end-1) {
+    if(call_stack[stack_p - 1].n == _end-1) { //  if(n == stack_0_size) {
       goto _done;
     }
 
@@ -107,7 +104,7 @@ _recursive_label: // void _recursive_label(size_t n);
        // call_stack[stack_p] = new(&buffer[stack_p * sizeof(CallFrame)]) CallFrame(call_stack[stack_p-1]->n+1);
        // stack_p++;
 
-       call_stack[stack_p++] = CallFrame(call_stack[stack_p-1].n+1);
+       call_stack[stack_p++] = CallFrame(call_stack[stack_p-1].n+1); // _recursive(n+1);
        goto _recursive_label;
        // AFTER_CODE?? See below
 
