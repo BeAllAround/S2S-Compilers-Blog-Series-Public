bf052e971776333165caf681056898faf83a5cda
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2026-04-06 04:27:03 +0200

    upload

---
+++ Recursion into Goto Labels Part III/main01.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+
+
+#include <vector>
+
+
+#include <cassert>
+
+
+struct CallFrame {
+
+    // args
+    struct Args {
+        int n;
+    } args;
+
+
+    // Return Vals
+    struct Ret {
+        void* origin { nullptr };
+        int value { 0 }; // RETURN VALUE
+        // int value { -1000 }; // NOTE: SHOULD WORK TOO
+    } ret;
+
+    CallFrame() {}
+
+    CallFrame(int n) {
+        args.n = n;
+    }
+
+
+    CallFrame(const CallFrame& other) {
+        args.n = other.args.n;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+    }
+
+
+    CallFrame(CallFrame&& other) {
+        args.n = std::move(other.args.n);
+        ret.origin = std::move(other.ret.origin); // NOT NEEDED FOR PRIMITIVE
+        ret.value = other.ret.value;
+    }
+
+    CallFrame& operator=(const CallFrame& other) {
+
+        args.n = other.args.n;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+
+        return *this;
+    }
+
+    CallFrame& operator=(CallFrame&& other) {
+        args.n = std::move(other.args.n);
+        ret.origin = std::move(other.ret.origin); // NOT NEEDED FOR PRIMITIVE
+        ret.value = other.ret.value;
+
+        return *this;
+    }
+
+    ~CallFrame() {}
+
+};
+
+
+/*
+// ASSEMBLY -O1 level (Cleanest assembly to read in my opinion)
+recursion01(int):
+        push    r14
+        push    rbx
+        sub     rsp, 24
+        cmp     edi, 1
+        je      .L17
+        cmp     edi, 2
+        je      .L18
+        mov     DWORD PTR [rsp+12], 3
+        mov     eax, DWORD PTR [rsp+12]
+        cmp     eax, 1
+        jle     .L4
+        mov     ebx, 1
+.L14:
+        mov     edi, ebx
+        call    recursion01(int)
+        add     ebx, 1
+        mov     eax, DWORD PTR [rsp+12]
+        cmp     eax, ebx
+        jg      .L14
+.L4:
+        add     rsp, 24
+        pop     rbx
+        pop     r14
+        ret
+*/
+
+void recursion01(int n) {
+    if(n == 1) {
+        std::cout << 1 << std::endl;
+        return;
+    } else if(n == 2) {
+        std::cout << 2 << std::endl;
+        return;
+    }
+
+    volatile int _end = 3; // prevent the compiler from optimizing the loop out
+
+    for(int i = 1; i < _end; i++) {
+        recursion01(i);
+    }
+
+}
+
+
+
+
+int _recursion01(int _n) {
+
+    // NOTE: CAN'T INLINE THIS FUNCTION DUE TO THE COMPUTED GOTO: YUCK!
+    // __label__ call1_origin;
+    // __label__ call2_origin;
+
+    // void *jmps_origin[] = { &&call1_origin, &&call2_origin };
+
+    // Can be dynamically allocated stack, which means we can call it as long as there is memory available
+    static CallFrame stack[20];
+
+    // static size_t stack_c = 0;
+    size_t stack_c = 0; // mov     QWORD PTR [rbp-24], 0
+
+
+    // PUSH
+    stack[stack_c] = CallFrame(_n);
+    stack_c++;
+
+
+    int i = 1;
+
+    // CALL
+    __recursion_01: {}
+
+    // function args
+    int n = stack[stack_c-1].args.n; // last: stack_c-1
+
+
+    { // FUNCTION BODY
+        if(n == 1) {
+            stack[stack_c-1].ret.value = 0; // return 0;
+            std::cout << 1 << std::endl;
+            goto _ret;
+        } else if(n == 2) {
+            stack[stack_c-1].ret.value = 0; // return 0;
+            std::cout << 2 << std::endl;
+            goto _ret;
+        }
+
+        while(i < 3) {
+            {
+                // NOTE: Limit the scope of a by declaring it within a block statement that ends before the label.
+                CallFrame call1 = CallFrame();
+                call1.args.n = i;
+                call1.ret.origin = &&call1_origin;
+                // PUSH
+                stack[stack_c++] = std::move(call1);
+
+                // CALL
+                goto __recursion_01;
+            }
+            // ORIGIN OF RET
+            call1_origin: {}
+
+
+           i++; 
+        }
+
+    }
+
+    _ret: {
+        // Stack is empty
+        /*
+        if(stack_c == 0) {
+            goto _done;
+            // return;
+        }
+        */
+
+        // pop rbx
+        stack_c--;
+
+        // NOTE: INITIAL PUSH ON LINE 137
+        if(stack_c == 0) {
+            goto _done;
+            // return;
+        }
+
+        // std::cout << "stack_c: " << stack_c << std::endl;
+
+        // ret
+        void* ret_origin = stack[stack_c].ret.origin;
+        goto *ret_origin;
+
+    }
+
+
+    _done: {}
+
+
+    assert(stack_c == 0);
+
+
+    return stack[stack_c].ret.value;
+
+}
+
+
+int main() {
+
+    recursion01(0);
+
+    _recursion01(0);
+
+
+
+
+    return 0;
+}
