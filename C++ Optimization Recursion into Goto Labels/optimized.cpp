9b7aad386c6d1a3a6c5a390c49e0567cbd3db53d
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2026-04-10 18:17:05 +0200

    upload

---
+++ C++ Optimization Recursion into Goto Labels/optimized.cpp
@@ -0,0 +1,490 @@
+#include <memory>
+
+#include <iostream>
+
+#include <cassert>
+
+
+#define start_time clock_t s_t_a_r_t = clock();
+
+#if __GNUG__
+#define end_time printf("[Cpu_time_used: %f]\n", static_cast<double>(clock() - s_t_a_r_t) / CLOCKS_PER_SEC);
+#else
+#define end_time printf("[Cpu_time_used: %f]\n", ((double) (clock() - s_t_a_r_t)) / CLOCKS_PER_SEC);
+#endif
+
+using namespace std;
+
+struct TreeNode {
+
+    int val { 0 };
+
+    shared_ptr<TreeNode> left { nullptr };
+    shared_ptr<TreeNode> right { nullptr };
+
+    TreeNode() {}
+
+    TreeNode(int val) : val{val} {}
+
+};
+
+
+struct CallFrame {
+
+    // args
+    struct Args {
+        const shared_ptr<TreeNode>* node { nullptr };
+    } args;
+
+
+    // Return Vals
+    struct Ret {
+        void* origin { nullptr };
+        int value { 0 }; // RETURN VALUE
+        // int value { -1000 }; // NOTE: SHOULD WORK TOO
+    } ret;
+
+    CallFrame() {}
+
+
+    CallFrame(const CallFrame& other) {
+        args.node = other.args.node;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+    }
+
+
+    CallFrame(CallFrame&& other) {
+        args.node = other.args.node;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+    }
+
+    CallFrame& operator=(const CallFrame& other) {
+
+        args.node = other.args.node;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+
+        return *this;
+    }
+
+    CallFrame& operator=(CallFrame&& other) {
+        args.node = other.args.node;
+        ret.origin = other.ret.origin;
+        ret.value = other.ret.value;
+
+        return *this;
+    }
+
+    ~CallFrame() {}
+
+};
+
+template<class T, size_t Size>
+class StaticStorage {
+  public:
+  using ItemType = T;
+  unsigned char buffer[sizeof(T) * Size];
+  size_t stack_count = 0;
+
+  StaticStorage() {}
+
+
+  // NOTE: Deleted for now. Will think about this
+  StaticStorage(const StaticStorage&) = delete;
+  StaticStorage(StaticStorage&&) = delete;
+
+  StaticStorage& operator=(const StaticStorage&) = delete;
+  StaticStorage& operator=(StaticStorage&&) = delete;
+
+  void add(const T& item) {
+    new(&buffer[stack_count * sizeof(T)]) T(item);
+    stack_count++;
+  }
+
+  void add(T&& item) {
+    new(&buffer[stack_count * sizeof(T)]) T(std::move(item));
+    stack_count++;
+  }
+
+
+  // T* pop();
+  // NOTE: POP FOR STATIC STORAGE IS ILL_ADVISED, RESULTING IN MEMORY LEAKS (EVEN WHEN THERE IS A CAST, WE ARE STILL POINTINT TO UCHAR BUFFER)
+  // MAKE SURE TO TEST THIS AT the -O0 level optimizations CUZ THE COMPILER JUST IS TAKES STUFF OUT AT O3
+  T& pop() { // SAME GOES FOR const T&
+    stack_count--;
+    T& popped = *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T)));
+
+    // *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T))) = T();
+
+
+    return popped;
+  }
+
+  T& at(size_t i) {
+    return *reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+  }
+
+  T& last() {
+    return *reinterpret_cast<T*>(buffer + ( (stack_count-1) * sizeof(T)));
+  }
+
+  // TODO: WRAP UP IN A MACRO JUST LIKE _storage_add
+  void insert(size_t i, T&& item) {
+    T* _at = reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+    *_at = std::move(item);
+    // ALTERNATIVELY
+    // _at->~T();
+    // new(&buffer[i * sizeof(T)]) T(std::move(item));
+  }
+
+
+  ~StaticStorage() {
+    for(size_t i = 0; i < stack_count; i++) {
+      reinterpret_cast<T*>(buffer + (i * sizeof(T)))->~T();
+    }
+  }
+
+
+};
+
+#define _storage_add(storage, item, T) \
+    new(&storage.buffer[storage.stack_count * sizeof(T)]) item; \
+    storage.stack_count++;
+
+// synonym
+#define _storage_push _storage_add
+
+
+
+/*
+// RECURSION PSEUDO CODE
+func recursive_tree_sum(root: Node) {
+    if(root == nullptr) {
+        return 0;
+    }
+    
+    return root.val + recursive_tree_sum(root.left) + recursive_tree_sum(root.right);
+}
+*/
+
+int _recursive_sum_tree(const shared_ptr<TreeNode>& node) {
+
+    if(node.get() == nullptr) {
+        return 0;
+    }
+
+    /*
+    // Less perplexing to translate if we are look at this like the following:
+    // It is also the most efficient to look at the assembly output that the compiler generates for it below.
+    int n = node.get()->val;
+    int n1 = _recursive_sum_tree(node.get()->left);
+    int n2 = _recursive_sum_tree(node.get()->right);
+    return n + n1 + n2;
+    */
+
+    return node.get()->val + _recursive_sum_tree(node.get()->left) + _recursive_sum_tree(node.get()->right);
+
+}
+
+/*
+// ASSEMBLY
+_recursive_sum_tree(std::shared_ptr<TreeNode> const&):
+        push    rbp
+        push    rbx
+        sub     rsp, 8
+        mov     rbp, QWORD PTR [rdi]
+        mov     eax, 0
+        test    rbp, rbp
+        je      .L15
+        mov     ebx, DWORD PTR [rbp+0]
+        lea     rdi, [rbp+8]
+        call    _recursive_sum_tree(std::shared_ptr<TreeNode> const&)
+        add     ebx, eax
+        lea     rdi, [rbp+24]
+        call    _recursive_sum_tree(std::shared_ptr<TreeNode> const&)
+        add     eax, ebx
+.L15:
+        add     rsp, 8
+        pop     rbx
+        pop     rbp
+        ret
+*/
+
+inline CallFrame make_call1(const shared_ptr<TreeNode>*, void*) __attribute__((always_inline));
+
+inline CallFrame make_call1(const shared_ptr<TreeNode>* left, void* call1_origin) {
+    CallFrame call1 = CallFrame();
+    call1.args.node = left;
+    call1.ret.origin = call1_origin;
+
+    // Copy Elision
+    return call1;
+
+}
+
+inline CallFrame make_call2(const shared_ptr<TreeNode>*, void*) __attribute__((always_inline));
+
+inline CallFrame make_call2(const shared_ptr<TreeNode>* right, void* call2_origin) {
+    CallFrame call2 = CallFrame();
+    call2.args.node = right;
+    call2.ret.origin = call2_origin;
+
+    // Copy Elision
+    return call2;
+
+}
+
+// TRANSLATION OF THE FUNCTION ABOVE
+int _sum_tree(const shared_ptr<TreeNode>& _node) {
+
+    // NOTE: CAN'T INLINE THIS FUNCTION DUE TO THE COMPUTED GOTO: YUCK!
+    // __label__ call1_origin;
+    // __label__ call2_origin;
+
+    // void *jmps_origin[] = { &&call1_origin, &&call2_origin };
+
+    // Can be dynamically allocated stack, which means we can call it as long as there is memory available
+    StaticStorage<CallFrame, 20> stack;
+    // static StaticStorage<CallFrame, 20> stack;
+
+
+    CallFrame initial_call = CallFrame();
+    initial_call.args.node = &_node;
+    // PUSH
+    _storage_push(stack, CallFrame(std::move(initial_call)), CallFrame);
+
+
+    // CALL
+    __sum_tree: {}
+
+    // function args
+    const shared_ptr<TreeNode>& node = *stack.last().args.node; // last: stack_c-1
+
+    { // FUNCTION BODY
+        if(node.get() == nullptr) {
+            stack.last().ret.value = 0; // return 0;
+            goto _ret;
+        }
+
+        // NOTE: DUE TO LABEL AND COMPUTED JUMPS, IT IS REQUIRED TO ENCLOSE THESE BLOCKS IN C++ DUE TO SKIPPING INITIALIZATION AND C++ RAII. THIS ALSO MAKES SENSE AT ASSEMBLY LEVEL
+        // _print_in_order(node.get()->left);
+        {
+            // NOTE: Limit the scope of a by declaring it within a block statement that ends before the label.
+            // REFERENCE and KUDOS TO: https://stackoverflow.com/a/70944477/14102601
+            /*
+            const shared_ptr<TreeNode>& node = *stack.last().args.node; // last: stack_c-1
+            CallFrame call1 = CallFrame();
+            call1.args.node = &node.get()->left;
+            call1.ret.origin = &&call1_origin;
+            // PUSH
+            _storage_push(stack, CallFrame(std::move(call1)), CallFrame);
+            // ~CallFrame()
+            // CallFrame(CallFrame&&)
+            */
+
+            _storage_push(stack, CallFrame(make_call1(&node.get()->left, &&call1_origin)), CallFrame);
+
+            // CALL
+            goto __sum_tree;
+        }
+        // ORIGIN OF RET
+        call1_origin: {}
+        // ORIGIN_RET = stack[stack_c].ret.value;
+
+        {
+            // NOTE: NEEDS TO BE HERE SINCE AFTER COMPUTED GOTO TO CALL1_ORIGIN: THE NODE ABOVE IS LOST
+            const shared_ptr<TreeNode>& node = *stack.last().args.node; // last: stack_c-1 // mov     rax, QWORD PTR [rbp-24]
+
+            // C++: return node.get()->val + _recursive_sum_tree(node.get()->left)
+            stack.last().ret.value = node.get()->val + stack.at(stack.stack_count).ret.value;
+        }
+
+        // _print_in_order(node.get()->right);
+        {
+            const shared_ptr<TreeNode>& node = *stack.last().args.node;
+
+            /*
+            CallFrame call2 = CallFrame();
+
+            call2.args.node = &node.get()->right;
+            call2.ret.origin = &&call2_origin;
+            */
+
+            // PUSH
+            _storage_push(stack, CallFrame(make_call2(&node.get()->right, &&call2_origin)), CallFrame);
+
+            // CALL
+            goto __sum_tree;
+        }
+        // ORIGIN OF RET
+        call2_origin: {}
+        // ORIGIN_RET = stack[stack_c].ret.value;
+
+        {
+            // C++: return _recursive_sum_tree(node.get()->left) + _recursive_sum_tree(node.get()->right);
+            stack.last().ret.value = stack.last().ret.value + stack.at(stack.stack_count).ret.value;
+        }
+
+    }
+
+    _ret: {
+        // Stack is empty
+        /*
+        if(stack_c == 0) {
+            goto _done;
+            // return;
+        }
+        */
+
+        // pop rbx
+        stack.pop();
+
+        // NOTE: INITIAL PUSH ON LINE 137
+        if(stack.stack_count == 0) {
+            goto _done;
+            // return;
+        }
+
+        // std::cout << "stack_c: " << stack_c << std::endl;
+
+        // ret
+        void* ret_origin = stack.at(stack.stack_count).ret.origin;
+        goto *ret_origin;
+
+    }
+
+
+    _done: {}
+
+
+    // ONLY FOR UNIT TESTS
+    // assert(stack.stack_count == 0);
+
+
+    return stack.at(stack.stack_count).ret.value;
+
+}
+
+
+
+
+#define _SIZE 10000
+
+shared_ptr<TreeNode> _make_root() {
+    shared_ptr<TreeNode> root = make_shared<TreeNode>(4);
+
+    root.get()->left = make_shared<TreeNode>(2);
+    root.get()->right = make_shared<TreeNode>(5);
+
+    root.get()->left.get()->left = make_shared<TreeNode>(1);
+    root.get()->left.get()->right = make_shared<TreeNode>(3);
+
+    root.get()->right.get()->left = make_shared<TreeNode>(6);
+    root.get()->right.get()->right = make_shared<TreeNode>(7);
+
+    root.get()->right.get()->left->left = make_shared<TreeNode>(0);
+    root.get()->right.get()->left->right = make_shared<TreeNode>(-1);
+
+    root.get()->right.get()->right->left = make_shared<TreeNode>(8);
+    root.get()->right.get()->right->right = make_shared<TreeNode>(9);
+
+    root.get()->right.get()->right->right->left = make_shared<TreeNode>(77);
+    root.get()->right.get()->right->right->right = make_shared<TreeNode>(88);
+
+
+    root.get()->right.get()->left->left->left = make_shared<TreeNode>(99);
+    root.get()->right.get()->left->left->right = make_shared<TreeNode>(111);
+
+
+    return root;
+}
+
+
+int main1() {
+
+    shared_ptr<TreeNode> root = _make_root();
+    // 415
+
+
+    int _result = _sum_tree(root);
+
+    std::cout << "_result: " << _result << std::endl;
+
+    assert(_result == _recursive_sum_tree(root));
+
+    root = make_shared<TreeNode>(4);
+    _result = _sum_tree(root);
+
+    std::cout << "_result: " << _result << std::endl;
+
+    assert(_result == _recursive_sum_tree(root));
+
+
+    return 0;
+}
+
+
+
+
+void benchmark_01() {
+    shared_ptr<TreeNode> root = _make_root();
+
+    start_time;
+    for(size_t i = 0; i < _SIZE; i++) {
+        volatile int _s = _recursive_sum_tree(root);
+    }
+
+    end_time;
+
+
+}
+
+
+void benchmark_02() {
+    shared_ptr<TreeNode> root = _make_root();
+
+    start_time;
+    for(size_t i = 0; i < _SIZE; i++) {
+        volatile int _s = _sum_tree(root);
+    }
+
+    end_time;
+
+}
+
+
+int main() {
+
+    /*
+    shared_ptr<TreeNode> root = make_shared<TreeNode>(4);
+
+
+    root.get()->left = make_shared<TreeNode>(2);
+    root.get()->right = make_shared<TreeNode>(5);
+
+    root.get()->left.get()->left = make_shared<TreeNode>(1);
+    root.get()->left.get()->right = make_shared<TreeNode>(3);
+
+    root.get()->right.get()->left = make_shared<TreeNode>(6);
+    root.get()->right.get()->right = make_shared<TreeNode>(7);
+
+
+    print_inorder(root);
+    std::cout << std::endl;
+
+
+    _print_order(root);
+    std::cout << std::endl;
+    */
+
+
+    benchmark_01();
+
+    benchmark_02();
+
+
+    return main1();
+
+}
---
+++ C++ QuickSort Recursion Goto Labels/main.cpp
@@ -0,0 +1,503 @@
+/* C implementation QuickSort */
+// https://gist.github.com/adwiteeya3/f1797534506be672b591f465c3366643
+
+
+#include <iostream>
+
+#include <cstdio>
+
+#include <cassert>
+
+template<class T, size_t Size>
+class StaticStorage {
+  public:
+  using ItemType = T;
+  unsigned char buffer[sizeof(T) * Size];
+  size_t stack_count = 0;
+
+  StaticStorage() {}
+
+
+  // NOTE: Deleted for now. Will think about this
+  StaticStorage(const StaticStorage&) = delete;
+  StaticStorage(StaticStorage&&) = delete;
+
+  StaticStorage& operator=(const StaticStorage&) = delete;
+  StaticStorage& operator=(StaticStorage&&) = delete;
+
+  void add(const T& item) {
+    new(&buffer[stack_count * sizeof(T)]) T(item);
+    stack_count++;
+  }
+
+  void add(T&& item) {
+    new(&buffer[stack_count * sizeof(T)]) T(std::move(item));
+    stack_count++;
+  }
+
+
+  // T* pop();
+  // NOTE: POP FOR STATIC STORAGE IS ILL_ADVISED, RESULTING IN MEMORY LEAKS (EVEN WHEN THERE IS A CAST, WE ARE STILL POINTINT TO UCHAR BUFFER)
+  // MAKE SURE TO TEST THIS AT the -O0 level optimizations CUZ THE COMPILER JUST IS TAKES STUFF OUT AT O3
+  T& pop() { // SAME GOES FOR const T&
+    stack_count--;
+    T& popped = *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T)));
+
+    // *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T))) = T();
+
+
+    return popped;
+  }
+
+  T& at(size_t i) {
+    return *reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+  }
+
+  T& last() {
+    return *reinterpret_cast<T*>(buffer + ( (stack_count-1) * sizeof(T)));
+  }
+
+  // TODO: WRAP UP IN A MACRO JUST LIKE _storage_add
+  void insert(size_t i, T&& item) {
+    T* _at = reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+    *_at = std::move(item);
+    // ALTERNATIVELY
+    // _at->~T();
+    // new(&buffer[i * sizeof(T)]) T(std::move(item));
+  }
+
+
+  ~StaticStorage() {
+    for(size_t i = 0; i < stack_count; i++) {
+      reinterpret_cast<T*>(buffer + (i * sizeof(T)))->~T();
+    }
+  }
+
+
+};
+
+#define _storage_add(storage, item, T) \
+    new(&storage.buffer[storage.stack_count * sizeof(T)]) item; \
+    storage.stack_count++;
+
+// synonym
+#define _storage_push _storage_add
+
+
+
+inline void swap(int*, int*) __attribute__((always_inline));
+
+inline void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+inline int partition(int*, int, int) __attribute__((always_inline));
+
+inline int partition(int* arr, int low, int high)
+{
+	int pivot = arr[high]; 
+	int i = (low - 1);	   
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++; 
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+/*
+quickSort(int*, int, int):
+        cmp     esi, edx
+        jge     .L7
+        push    r12
+        push    rbp
+        push    rbx
+        mov     rbp, rdi
+        mov     r12d, edx
+        movsx   rax, edx
+        lea     r10, [rdi+rax*4]
+        mov     r8d, DWORD PTR [r10]
+        lea     ebx, [rsi-1]
+        movsx   rcx, esi
+        lea     rax, [rdi+rcx*4]
+        sub     edx, esi
+        add     rdx, rcx
+        lea     rdi, [rdi+rdx*4]
+        jmp     .L4
+.L3:
+        add     rax, 4
+        cmp     rax, rdi
+        je      .L10
+.L4:
+        mov     edx, DWORD PTR [rax]
+        cmp     r8d, edx
+        jle     .L3
+        add     ebx, 1
+        movsx   rcx, ebx
+        lea     rcx, [rbp+0+rcx*4]
+        mov     r9d, DWORD PTR [rcx]
+        mov     DWORD PTR [rcx], edx
+        mov     DWORD PTR [rax], r9d
+        jmp     .L3
+.L10:
+        movsx   rax, ebx
+        lea     rax, [rbp+4+rax*4]
+        mov     edx, DWORD PTR [rax]
+        mov     ecx, DWORD PTR [r10]
+        mov     DWORD PTR [rax], ecx
+        mov     DWORD PTR [r10], edx
+        mov     edx, ebx
+        mov     rdi, rbp
+        call    quickSort(int*, int, int)
+        lea     esi, [rbx+2]
+        mov     edx, r12d
+        mov     rdi, rbp
+        call    quickSort(int*, int, int)
+        pop     rbx
+        pop     rbp
+        pop     r12
+        ret
+.L7:
+        ret
+*/
+
+void quickSort(int* arr, int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+struct CallFrame {
+
+    // args
+    struct Args {
+        int* arr { nullptr };
+        int low;
+        int high;
+    } args;
+
+	// Body Variables such as pi that need to be in the current call frame as well. See void quickSort(int*, int, int) above
+	struct Body {
+		int pi;
+	} vars;
+
+
+    // Return Vals
+    struct Ret {
+        void* origin { nullptr };
+		// NOTE: quickSort return VOID so no return values NEEDED
+    } ret;
+
+    CallFrame() {}
+
+
+    CallFrame(const CallFrame& other) {
+        args.arr = other.args.arr;
+        args.low = other.args.low;
+        args.high = other.args.high;
+
+        ret.origin = other.ret.origin;
+    }
+
+
+    CallFrame(CallFrame&& other) {
+        args.arr = other.args.arr;
+        args.low = other.args.low;
+        args.high = other.args.high;
+
+        ret.origin = other.ret.origin;
+    }
+
+    CallFrame& operator=(const CallFrame& other) {
+
+        args.arr = other.args.arr;
+        args.low = other.args.low;
+        args.high = other.args.high;
+
+        ret.origin = other.ret.origin;
+
+        return *this;
+    }
+
+    CallFrame& operator=(CallFrame&& other) {
+        args.arr = other.args.arr;
+        args.low = other.args.low;
+        args.high = other.args.high;
+
+        ret.origin = other.ret.origin;
+
+        return *this;
+    }
+
+    ~CallFrame() {}
+
+};
+
+// TRANSLATION OF THE FUNCTION ABOVE
+void _quickSort(int* _arr, int _low, int _high) {
+
+    // NOTE: CAN'T INLINE THIS FUNCTION DUE TO THE COMPUTED GOTO: YUCK!
+    // __label__ call1_origin;
+    // __label__ call2_origin;
+
+    // void *jmps_origin[] = { &&call1_origin, &&call2_origin };
+
+    // Can be dynamically allocated stack, which means we can call it as long as there is memory available
+    StaticStorage<CallFrame, 100> stack;
+    // static StaticStorage<CallFrame, 20> stack;
+
+
+    CallFrame initial_call = CallFrame();
+    initial_call.args.arr = _arr;
+    initial_call.args.low = _low;
+    initial_call.args.high = _high;
+
+    // PUSH
+    _storage_push(stack, CallFrame(std::move(initial_call)), CallFrame);
+
+
+    // CALL
+    __quickSort: {}
+
+    // function args
+	int* arr = stack.last().args.arr;
+
+	int low = stack.last().args.low;
+	int high = stack.last().args.high;
+
+
+    { // FUNCTION BODY
+
+		if(low < high) {
+			int pi = partition(arr, low, high);
+
+			// push vars
+			stack.last().vars.pi = pi;
+
+			{
+				CallFrame call1 = CallFrame();
+				call1.args.arr = arr;
+				call1.args.low = low;
+				call1.args.high = pi - 1;
+				call1.ret.origin = &&call1_origin;
+            	// PUSH CALL
+            	_storage_push(stack, CallFrame(std::move(call1)), CallFrame);
+				// CALL
+				goto __quickSort;
+			}
+			// ORIGIN OF RET
+        	call1_origin: {}
+
+			{
+				int pi = stack.last().vars.pi;
+				int* arr = stack.last().args.arr;
+
+				int high = stack.last().args.high;
+				CallFrame call2 = CallFrame();
+				call2.args.arr = arr;
+				call2.args.low = pi + 1;
+				call2.args.high = high;
+				call2.ret.origin = &&call2_origin;
+            	// PUSH
+            	_storage_push(stack, CallFrame(std::move(call2)), CallFrame);
+				// CALL
+				goto __quickSort;
+			}
+			// ORIGIN OF RET
+        	call2_origin: {}
+
+		}
+
+		goto _ret; // CAN BE JUST REMOVED
+
+    }
+
+    _ret: {
+        // Stack is empty
+        /*
+        if(stack_c == 0) {
+            goto _done;
+            // return;
+        }
+        */
+
+        // pop rbx
+        stack.pop();
+
+        // NOTE: INITIAL PUSH ON LINE 137
+        if(stack.stack_count == 0) {
+            goto _done;
+            // return;
+        }
+
+        // std::cout << "stack_c: " << stack_c << std::endl;
+
+        // ret
+        void* ret_origin = stack.at(stack.stack_count).ret.origin;
+        goto *ret_origin;
+
+    }
+
+
+    _done: {}
+
+
+    // ONLY FOR UNIT TESTS
+    // assert(stack.stack_count == 0);
+
+
+    return;
+
+}
+
+
+void printArray(int* arr, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+    std::cout << arr[i] << ' ';
+  std::cout << std::endl;
+}
+
+
+int main01() {
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printArray(arr, n);
+
+	quickSort(arr, 0, n - 1);
+
+	printf("Sorted array: \n");
+	printArray(arr, n);
+
+	return 0;
+}
+
+int main02() {
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printArray(arr, n);
+
+	_quickSort(arr, 0, n - 1);
+
+	printf("Sorted array: \n");
+	printArray(arr, n);
+
+	return 0;
+}
+
+void assertSorted(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        assert(arr[i] >= arr[i - 1]);
+    }
+}
+
+// Common Test Scenarios
+int main03() {
+
+    // 1. Basic test (your original)
+    int t1[] = {10, 7, 8, 9, 1, 5};
+
+    // 2. Already sorted
+    int t2[] = {1, 2, 3, 4, 5, 6};
+
+    // 3. Reverse sorted (worst case for some implementations)
+    int t3[] = {6, 5, 4, 3, 2, 1};
+
+    // 4. All elements same
+    int t4[] = {5, 5, 5, 5, 5};
+
+    // 5. Single element
+    int t5[] = {42};
+
+    // 6. Two elements (sorted)
+    int t6[] = {1, 2};
+
+    // 7. Two elements (unsorted)
+    int t7[] = {2, 1};
+
+    // 8. Includes negative numbers
+    int t8[] = {-3, -1, -7, -4, -5, -2};
+
+    // 9. Mixed positive + negative
+    int t9[] = {-10, 0, 5, -3, 8, -2, 1};
+
+    // 10. Large duplicates
+    int t10[] = {3, 1, 2, 3, 3, 3, 0, 3};
+
+    // 11. Zeros only
+    int t11[] = {0, 0, 0, 0};
+
+    // 12. Large values
+    int t12[] = {2147483647, -2147483648, 0, 999999, -999999};
+
+    // 13. Random order
+    int t13[] = {12, 4, 55, 23, 19, 1, 0, 87, 33};
+
+    // 14. Nearly sorted (common real-world case)
+    int t14[] = {1, 2, 3, 5, 4, 6, 7};
+
+    // 15. Alternating high/low
+    int t15[] = {1, 100, 2, 99, 3, 98, 4, 97};
+
+    // Put all tests into arrays of pointers
+    int* tests[] = {
+        t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15
+    };
+
+    int sizes[] = {
+        sizeof(t1)/sizeof(int),
+        sizeof(t2)/sizeof(int),
+        sizeof(t3)/sizeof(int),
+        sizeof(t4)/sizeof(int),
+        sizeof(t5)/sizeof(int),
+        sizeof(t6)/sizeof(int),
+        sizeof(t7)/sizeof(int),
+        sizeof(t8)/sizeof(int),
+        sizeof(t9)/sizeof(int),
+        sizeof(t10)/sizeof(int),
+        sizeof(t11)/sizeof(int),
+        sizeof(t12)/sizeof(int),
+        sizeof(t13)/sizeof(int),
+        sizeof(t14)/sizeof(int),
+        sizeof(t15)/sizeof(int)
+    };
+
+    int numTests = sizeof(tests) / sizeof(tests[0]);
+
+    for (int i = 0; i < numTests; i++) {
+        printf("\nTest %d:\nBefore: ", i + 1);
+        printArray(tests[i], sizes[i]);
+
+        _quickSort(tests[i], 0, sizes[i] - 1);
+
+        printf("After:  ");
+        printArray(tests[i], sizes[i]);
+        
+        assertSorted(tests[i], sizes[i]);
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+	return main03();
+
+}
