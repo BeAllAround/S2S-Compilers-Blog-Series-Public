09e45406c6f1c729ac1990fc982e21e19ef1c719
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2026-04-10 14:36:19 +0200

    update

--- C++ Static Storage Implementation/StaticStorage.cpp
+++ C++ Static Storage Implementation/StaticStorage.cpp
@@ -109,10 +109,29 @@ class StaticStorage {
 
 
   // T* pop();
-
-  T& pop() {
+  // NOTE: POP FOR STATIC STORAGE IS ILL_ADVISED, RESULTING IN MEMORY LEAKS (EVEN WHEN THERE IS A CAST, WE ARE STILL POINTINT TO UCHAR BUFFER)
+  // MAKE SURE TO TEST THIS AT the -O0 level optimizations CUZ THE COMPILER JUST IS TAKES STUFF OUT AT O3
+  T& pop() { // SAME GOES FOR const T&
     stack_count--;
-    return *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T)));
+    T& popped = *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T)));
+
+    // *reinterpret_cast<T*>(buffer + ( stack_count * sizeof(T))) = T();
+
+
+    return popped;
+  }
+
+  const T& at(size_t i) {
+    return *reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+  }
+
+  // TODO: WRAP UP IN A MACRO JUST LIKE _storage_add
+  void insert(size_t i, T&& item) {
+    T* _at = reinterpret_cast<T*>(buffer + ( i * sizeof(T)));
+    *_at = std::move(item);
+    // ALTERNATIVELY
+    // _at->~T();
+    // new(&buffer[i * sizeof(T)]) T(std::move(item));
   }
 
 
@@ -125,10 +144,11 @@ class StaticStorage {
 
 };
 
-
 #define _storage_add(storage, item, T) \
     new(&storage.buffer[storage.stack_count * sizeof(T)]) item; \
-    storage.stack_count++; \
+    storage.stack_count++;
+
+
 
 
 
@@ -145,8 +165,9 @@ int main1() {
   _storage_add(storage, S(3), S);
 
 
-  S s = std::move(storage.pop());
-  assert(*s.i_ptr == 3);
+  // S s = std::move(storage.pop()); // OK, ACTUALLY
+  // assert(*s.i_ptr == 3);
+  assert(*storage.at(0).i_ptr == 3);
 
   return 0;
 }
