29d58151507c4810dea274b307987066c6de7ab5
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2026-05-18 23:29:19 +0200

    update

--- C++ Hacking Destructor Semantics Part II/main02.cpp
+++ C++ Hacking Destructor Semantics Part II/main02.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <cassert>
+#include <cstring>
+
 
 
 
@@ -12,8 +15,8 @@ const char* return_C_string() {
 
   U s = {};
 
-  new(&s.s) std::string("AD"); // Small string optimization in this case results in undefined behaviour
-  s.s.reserve(100); // FIXES THE SMALL STRING OPTIMIZATION HURDLE // ALSO: COMMENT THIS OUT AND COMPARE THE OUTPUTS/RESULTS WITH -O0, -O1, -O2, -O3 level optimizations
+  new(&s.s) std::string("AD"); // NOTE: Small string optimization in this case results in undefined behaviour
+  s.s.reserve(100); // NOTE: FIXES THE SMALL STRING OPTIMIZATION HURDLE // ALSO: COMMENT THIS OUT AND COMPARE THE OUTPUTS/RESULTS WITH -O0, -O1, -O2, -O3 level optimizations
 
   std::cout << s.s.c_str() << std::endl;
 
@@ -26,6 +29,8 @@ int main() {
 
 
   std::cout << c_string << std::endl;
+
+  assert(strcmp(c_string, "AD") == 0);
 
   // TODO: HOW TO DESTROY c_string in this case
   
